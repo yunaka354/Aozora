a15223a1e18f8beb0f759e212798465bef00a5cb
yunaka354 <yuichi.nakamura86@gmail.com>
2024-02-18 22:16:05 -0500

    add tweet endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,11 +2,12 @@ mod api;
 mod model;
 use axum::response::IntoResponse;
 use axum::response::Json;
-use axum::routing::get;
+use axum::routing::{get, post};
 use axum::Extension;
 use axum::Router;
 use http::Method;
 use tower_http::cors::{Any, CorsLayer};
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() {
@@ -24,6 +25,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(root))
         .route("/timeline", get(timeline))
+        .route("/tweet", post(tweet))
         .layer(cors)
         .layer(Extension(api));
 
@@ -40,4 +42,9 @@ async fn root() -> &'static str {
 async fn timeline(Extension(api): Extension<api::API>) -> impl IntoResponse {
     let timeline = api.get_timeline().await;
     Json(timeline)
+}
+
+async fn tweet(Json(payload): Json<Value>) -> impl IntoResponse {
+    println!("{}", payload);
+    Json(payload)
 }
